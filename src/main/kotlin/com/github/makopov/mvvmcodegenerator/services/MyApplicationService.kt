6abac8698d18699e52e1c151408092ef2d7009a4
GitHub Action <action@github.com>
2020-12-28 12:45:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/makopov/mvvmcodegenerator/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/makopov/mvvmcodegenerator/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.makopov.mvvmcodegenerator.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.makopov.mvvmcodegenerator.MyBundle
 
 class MyApplicationService {
 
