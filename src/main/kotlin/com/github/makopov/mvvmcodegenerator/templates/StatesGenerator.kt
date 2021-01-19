669d75d512072e91e9e4ad03d7438f3e1d3b1e4e
Artem Leshchenko <artem@app3null.com>
2021-01-19 16:58:04 +0200

    removed redundant parameter

--- src/main/kotlin/com/github/makopov/mvvmcodegenerator/templates/StatesGenerator.kt
+++ src/main/kotlin/com/github/makopov/mvvmcodegenerator/templates/StatesGenerator.kt
@@ -28,7 +28,7 @@ import com.app3null.basestructure.actions.ViewStateAction
 import com.app3null.basestructure.models.DisposableValue
 import $packageName.${entityName}ViewState
 
-class ShowLoaderAction(private val throwable: Throwable) : ViewStateAction<${entityName}ViewState> {
+class ShowLoaderAction : ViewStateAction<${entityName}ViewState> {
     override fun newState(oldState: ${entityName}ViewState): ${entityName}ViewState {
         return oldState.copy(showLoader = DisposableValue(true))
     }
